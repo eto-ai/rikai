29fed17c4ad2188888ee7322c376bae1f5da66c3
Lei Xu <eddyxu@gmail.com>
2021-01-08 21:38:11 -0800

    use case object to expose PointType and OrientionType that is compatibile with Spark types
    test equality of two points
    
    add orientation equality

--- src/main/scala/org/apache/spark/sql/rikai/Orientation.scala
+++ src/main/scala/org/apache/spark/sql/rikai/Orientation.scala
@@ -13,77 +13,78 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.spark.sql.rikai
 
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.catalyst.expressions.GenericInternalRow
 import org.apache.spark.sql.catalyst.InternalRow
 
-import Utils.approxEqual
+import Utils._
 
 /**
-  * A Point in 3-D space
-  *
-  * @constructor create a 3-D Point
+  * Object orientation in the 3D space
   */
-@SQLUserDefinedType(udt = classOf[PointType])
-class Point(
+@SQLUserDefinedType(udt = classOf[OrientationType])
+class Orientation(
     val x: Double,
     val y: Double,
-    val z: Double
+    val z: Double,
+    val w: Double
 ) {
-
-  override def equals(p: Any): Boolean =
-    p match {
-      case other: Point =>
+  override def equals(o: Any): Boolean =
+    o match {
+      case other: Orientation =>
         approxEqual(x, other.x) &&
           approxEqual(y, other.y) &&
-          approxEqual(z, other.z)
+          approxEqual(z, other.z) &&
+          approxEqual(w, other.z)
       case _ => false
     }
 
-  override def toString: String = f"Point($x, $y, $z)"
+  override def toString: String = s"Orientation(x=$x, y=$y, z=$z, w=$z)"
 }
 
-/**
-  * User defined type for 3-D Point
-  */
-private[rikai] class PointType extends UserDefinedType[Point] {
+private[spark] class OrientationType extends UserDefinedType[Orientation] {
 
   override def sqlType: DataType =
     StructType(
       Seq(
         StructField("x", DoubleType, nullable = false),
         StructField("y", DoubleType, nullable = false),
-        StructField("z", DoubleType, nullable = false)
+        StructField("z", DoubleType, nullable = false),
+        StructField("w", DoubleType, nullable = false)
       )
     )
 
-  override def pyUDT: String = "rikai.spark.types.geometry.PointType"
+  override def pyUDT: String = "rikai.spark.types.Orientation"
 
-  override def serialize(obj: Point): Any = {
-    val row = new GenericInternalRow(3)
+  override def serialize(obj: Orientation): Any = {
+    val row = new GenericInternalRow(4)
     row.setDouble(0, obj.x)
     row.setDouble(1, obj.y)
     row.setDouble(2, obj.z)
+    row.setDouble(3, obj.w)
     row
   }
 
-  override def deserialize(datum: Any): Point =
+  override def deserialize(datum: Any): Orientation = {
     datum match {
       case row: InternalRow => {
         val x = row.getDouble(0)
         val y = row.getDouble(1)
         val z = row.getDouble(2)
-        new Point(x, y, z)
+        val w = row.getDouble(3)
+        new Orientation(x, y, z, w)
       }
     }
+  }
 
-  override def userClass: Class[Point] = classOf[Point]
+  override def userClass: Class[Orientation] = classOf[Orientation]
 
-  override def defaultSize: Int = 24
+  override def defaultSize: Int = 16
 
-  override def typeName: String = "Point"
+  override def typeName: String = "orientation"
 }
 
-case object PointType extends PointType
+case object OrientationType extends OrientationType

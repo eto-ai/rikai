cc7aa62066e815c98297606bf5b414c9c75a5ce8
Lei Xu <eddyxu@gmail.com>
2021-01-09 22:50:06 -0800

    add box3d class

--- python/rikai/types/geometry.py
+++ python/rikai/types/geometry.py
@@ -51,3 +51,54 @@ class Point(ToNumpy):
 
     def to_numpy(self) -> np.ndarray:
         return np.array([self.x, self.y, self.z])
+
+
+class Box3d(ToNumpy):
+    """A 3-D bounding box
+
+    Attributes
+    ----------
+    center : Point
+        Center point of the bounding box
+    length : float
+        The x dimention of the box
+    width : float
+        The y dimention of the box
+    height : float
+        The z dimention of the box
+    heading : float
+        The heading of the bounding box (in radians).  The heading is the angle
+        required to rotate +x to the surface normal of the box front face. It is
+        normalized to ``[-pi, pi)``.
+
+    References
+    ----------
+    .. [1] Waymo Dataset Spec https://github.com/waymo-research/waymo-open-dataset/blob/master/waymo_open_dataset/label.proto
+    """
+
+    def __init__(
+        self,
+        center: Point,
+        length: float,
+        width: float,
+        height: float,
+        heading: float,
+    ):
+        self.center = center
+        self.length = length
+        self.width = width
+        self.height = height
+        self.heading = heading
+
+    def __repr__(self) -> str:
+        return f"Box3d(center={self.center}, l={self.length}, h={self.height}, w={self.weight}, heading={self.heading})"
+
+    def __eq__(self, o: object) -> bool:
+        return (
+            isinstance(o, Box3d)
+            and self.center == o.center
+            and self.length == o.length
+            and self.width == o.width
+            and self.height == o.height
+            and self.heading == o.heading
+        )

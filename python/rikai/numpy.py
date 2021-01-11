2a25c79c59583258ff62f77ddcaeda13e6edad0f
Lei Xu <eddyxu@users.noreply.github.com>
2021-01-10 19:28:30 -0800

    Improve numpy doc (#7)
    * numpy doc update

--- python/rikai/numpy.py
+++ python/rikai/numpy.py
@@ -1,4 +1,3 @@
-#  Copyright 2020 Rikai Authors
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -12,11 +11,25 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-"""Wrappers and help functions to enable serialing numpy arrays transparently
-in Spark / Parquet / Pytorch
-"""
-from __future__ import absolute_import
+"""This module makes :py:class:`numpy.ndarray` inter-operatable with rikai
+from feature engineerings in `Spark <https://spark.apache.org/>`_ to be trained
+in Tensorflow and Pytorch.
 
+>>> # Feature Engineering in Spark
+>>> from rikai import numpy as np
+>>> df = spark.createDataFrame([Row(mask=np.array([1, 2, 3, 4]))])
+>>> df.write.format("rikai").save("s3://path/to/features")
+
+When use the rikai data in training, the serialized numpy data will be
+automatically converted into the appropriate format, i.e., :py:class:`torch.Tensor`
+in Pytorch:
+
+>>> from rikai.torch import DataLoader
+>>> data_loader = DataLoader("s3://path/to/features")
+>>> next(data_loader)
+{"mask": tensor([1, 2, 3])}
+
+"""
 
 # Third Party
 import numpy as np
@@ -29,31 +42,56 @@ __all__ = ["wrap", "array", "empty"]
 
 
 class ndarray(np.ndarray):  # pylint: disable=invalid-name
-    """This class extends numpy ndarray, with the capability to be serialized via Spark"""
+    """This class extends numpy ndarray, with the capability to be serialized in Spark"""
 
     __UDT__ = NDArrayType()
 
 
 def wrap(data: np.ndarray) -> np.ndarray:
-    """Wrap a numpy array to be able to work with spark and parquet"""
+    """Wrap a numpy array to be able to work Spark and Parquet.
+
+    Parameters
+    ----------
+    data : np.ndarray
+        A raw numpy array
+
+    Returns
+    -------
+    np.ndarray
+        A Numpy array that is compatible with Spark User Defined Type.
+
+
+    Example
+    -------
+    >>> import numpy as np
+    >>> from rikai.numpy import wrap
+    >>>
+    >>> arr = np.array([1, 2, 3], dtype=np.int64)
+    >>> df = spark.createDataFrame([Row(id=1, mask=wrap(arr))])
+    >>> df.write.format("rikai").save("s3://foo/bar")
+    """
     return data.view(ndarray)
 
 
 def array(obj, *args, **kwargs) -> np.ndarray:
-    """Create an numpy array
+    """Create an numpy array using the same API as :py:func:`numpy.array`.
 
     See Also
     --------
-    See :py:func:`numpy.array` for details
+
+    :py:func:`numpy.array`
     """
     return np.array(obj, *args, **kwargs).view(ndarray)
 
 
 def empty(shape, dtype=float, order="C") -> np.ndarray:
-    """Return a new array of giving shape and type, without initializing entries
+    """Return a new array of giving shape and type, without initializing entries.
+
+    The returned array can be directly used in a Spark :py:class:`~pyspark.sql.DataFrame`.
 
     See Also
     --------
-    See :py:func:`numpy.empty` for details
+
+    :py:func:`numpy.empty`
     """
     return wrap(np.empty(shape, dtype=dtype, order=order))

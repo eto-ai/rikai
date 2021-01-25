4ca2faf7c76f4c4fdc9ebe3fc32355b16a78a461
Lei Xu <eddyxu@gmail.com>
2021-01-24 20:35:44 -0800

    make NDArray display able in spark

--- src/main/scala/org/apache/spark/sql/rikai/NDArray.scala
+++ src/main/scala/org/apache/spark/sql/rikai/NDArray.scala
@@ -17,10 +17,44 @@
 package org.apache.spark.sql.rikai
 
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.catalyst.expressions.GenericInternalRow
+import org.apache.spark.sql.catalyst.InternalRow
 
+/**
+  * NDArray type. Storing the persisted data from numpy ndarray.
+  *
+  * Note that this class does not support interoperation in scala yet.
+  * It will only show a array summary in "df.show()".
+  * But the data is accessible from Pyspark and Pytorch / Tensorflow readers.
+  *
+  * @param dtype
+  */
 @SQLUserDefinedType(udt = classOf[NDArrayType])
-class NDArray {
-  override def toString: String = s"ndarray()"
+class NDArray(val dtype: Byte) {
+
+  /**
+    * It will only display a summary using df.show().
+    */
+  override def toString: String = s"ndarray(${portableDtype}, ...)"
+
+  /**
+    * @see Python ``rikai.convert.PortableDataType``
+    */
+  def portableDtype: String = {
+    dtype match {
+      case 1  => "uint8"
+      case 2  => "uint16"
+      case 3  => "uint32"
+      case 4  => "uint64"
+      case 5  => "int8"
+      case 6  => "int16"
+      case 7  => "int32"
+      case 8  => "int64"
+      case 9  => "float"
+      case 10 => "double"
+      case 11 => "boolean"
+    }
+  }
 }
 
 private[spark] class NDArrayType extends UserDefinedType[NDArray] {
@@ -36,9 +70,20 @@ private[spark] class NDArrayType extends UserDefinedType[NDArray] {
 
   override def pyUDT: String = "rikai.spark.types.NDArrayType"
 
-  override def serialize(obj: NDArray): Any = ???
+  override def serialize(obj: NDArray): Any = {
+    val row = new GenericInternalRow(3)
+    row.setByte(0, obj.dtype)
+    row
+  }
 
-  override def deserialize(datum: Any): NDArray = ???
+  override def deserialize(datum: Any): NDArray = {
+    datum match {
+      case row: InternalRow => {
+        val dtype = row.getByte(0)
+        new NDArray(dtype)
+      }
+    }
+  }
 
   override def userClass: Class[NDArray] = classOf[NDArray]
 

8ed52d89e1005fb51ddff39f7087f887ec922799
Lei Xu <eddyxu@gmail.com>
2021-02-27 23:06:24 -0800

    filesystem based model

---
+++ src/main/scala/ai/eto/rikai/sql/model/fs/FileSystemModel.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 Rikai authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ai.eto.rikai.sql.model.fs
+
+import ai.eto.rikai.sql.model.{Model, Registry}
+import ai.eto.rikai.sql.spark.SparkRunnable
+import org.apache.spark.sql.catalyst.expressions.Expression
+
+/**
+  * FileSystem-based [[Model]].
+  *
+  * It supports the following
+  *
+  * @param name model name
+  * @param uri model uri
+  * @param registry
+  */
+class FileSystemModel(val name: String, val uri: String, val registry: Registry)
+    extends Model
+    with SparkRunnable {
+
+  /** Convert a [[ai.eto.rikai.sql.model.Model]] to a Spark Expression in Spark SQL's logical plan. */
+  override def asSpark(args: Seq[Expression]): Expression = {
+    null
+  }
+}
---
+++ src/main/scala/ai/eto/rikai/sql/model/fs/FileSystemRegistry.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 Rikai authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ai.eto.rikai.sql.model.fs
+
+import ai.eto.rikai.sql.model.{Model, Registry}
+
+/**
+  * FileSystem based on [[Registry]]
+  */
+class FileSystemRegistry extends Registry {
+
+  /**
+    * Resolve a Model from the specific URI.
+    *
+    * @param uri  is the model registry URI.
+    * @param name is an optional model name. If provided,
+    *             will create the [[Model]] with this name.
+    * @throws ModelNotFoundException if the model does not exist on the registry.
+    * @return [[Model]] if found.
+    */
+  override def resolve(uri: String, name: Option[String]): Model = ???
+}

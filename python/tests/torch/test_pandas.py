6ddb7557da9d0b9bda931239318b7400943ff62f
Lei Xu <eddyxu@gmail.com>
2021-03-15 15:49:54 -0700

    add tests to cover udt

--- python/tests/torch/test_pandas.py
+++ python/tests/torch/test_pandas.py
@@ -17,16 +17,31 @@ import pandas as pd
 import numpy as np
 
 from rikai.torch.pandas import PandasDataset
+from rikai.types.geometry import Box2d
 
 
 def test_pandas_dataframe():
     data = [{"id": i, "value": str(i * 10)} for i in range(10)]
-    df = pd.DataFrame(data=data)
+    df = pd.DataFrame(data)
 
     dataset = PandasDataset(df)
     assert len(dataset) == 10
-    print(list(dataset))
     assert list(dataset) == data
+
+
+def test_dataframe_with_udt():
+    data = [{"id": i, "box": Box2d(i, i + 1, i + 2, i + 3)} for i in range(10)]
+    df = pd.DataFrame(data)
+
+    dataset = PandasDataset(df)
+    assert len(dataset) == 10
+    expected = [
+        {"id": i, "box": np.array([i, i + 1, i + 2, i + 3])} for i in range(10)
+    ]
+    for i in range(10):
+        row = dataset[i]
+        assert row["id"] == expected[i]["id"]
+        assert np.array_equal(row["box"], expected[i]["box"])
 
 
 def test_pandas_series():
@@ -36,3 +51,11 @@ def test_pandas_series():
     dataset = PandasDataset(series)
     assert len(dataset) == 10
     assert np.array_equal(data, dataset)
+
+
+def test_pandas_series_with_udt():
+    data = pd.Series([Box2d(i, i + 1, i + 2, i + 3) for i in range(10)])
+    dataset = PandasDataset(data)
+    assert len(dataset) == 10
+    for i in range(10):
+        assert np.array_equal(dataset[i], np.array([i, i + 1, i + 2, i + 3]))

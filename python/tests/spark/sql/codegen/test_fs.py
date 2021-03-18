eeb2587d5e7ff14a80d6dcd7b1556e869c0cb37b
Lei Xu <eddyxu@gmail.com>
2021-03-18 09:26:46 -0700

    add tests and fix lint

---
+++ python/tests/spark/sql/codegen/test_fs.py
@@ -0,0 +1,69 @@
+#  Copyright 2021 Rikai Authors
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import pytest
+
+from rikai.spark.sql.codegen.fs import validate_spec
+from rikai.spark.sql.exceptions import SpecError
+
+
+def test_validate_yaml_spec():
+    validate_spec(
+        {
+            "version": "1.2",
+            "name": "test_yaml_model",
+            "schema": "long",
+            "model": {
+                "uri": "s3://bucket/to/model.pt",
+                "unspecified_field": True,
+            },
+            "options": {
+                "gpu": "true",
+                "batch_size": 123,
+            },
+        },
+    )
+
+
+def test_validate_misformed_spec():
+    with pytest.raises(SpecError):
+        validate_spec({})
+
+    with pytest.raises(SpecError, match=".*version' is a required property.*"):
+        validate_spec(
+            {
+                "name": "test_yaml_model",
+                "schema": "long",
+                "model": {"uri": "s3://foo/bar"},
+            },
+        )
+
+    with pytest.raises(SpecError, match=".*'model' is a required property.*"):
+        validate_spec(
+            {
+                "version": "1.0",
+                "name": "test_yaml_model",
+                "schema": "long",
+            },
+        )
+
+    with pytest.raises(SpecError, match=".*'uri' is a required property.*"):
+        validate_spec(
+            {
+                "version": "1.0",
+                "name": "test_yaml_model",
+                "schema": "long",
+                "model": {},
+            },
+        )

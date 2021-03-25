78ef09a398e0637f373be7df8235bea9fdac229e
Lei Xu <eddyxu@gmail.com>
2021-03-24 21:39:00 -0700

    fix typo

--- python/rikai/torch/vision.py
+++ python/rikai/torch/vision.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-"""`Torchvision`_ compatibile Dataset
+"""`Torchvision`_ compatible Dataset
 
 
 .. _torchvision: https://pytorch.org/vision/stable/index.html

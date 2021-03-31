d8f39f1bf3aa2010a298da58dc29b90bc42277d8
changhiskhan <changshe@gmail.com>
2021-03-30 20:44:26 -0700

    fix regex matching code and change notebook version ref to use_snapshot=True

--- python/rikai/spark/utils.py
+++ python/rikai/spark/utils.py
@@ -39,11 +39,11 @@ def get_default_jar_version(use_snapshot=True):
     use_snapshot: bool, default True
         If True then map `*dev0` versions to `-SNAPSHOT`
     """
-    pattern = re.compile(r'[\d]+.[\d]+.[\d]+')
-    match = pattern.match(version)
+    pattern = re.compile(r'([\d]+.[\d]+.[\d]+)')
+    match = re.search(pattern, version)
     if not match:
         raise ValueError('Ill-formed version string {}'.format(version))
-    match_str = match.string
-    if use_snapshot and ((match_str) < len(version)):
+    match_str = match.group(1)
+    if use_snapshot and (len(match_str) < len(version)):
         return match_str + '-SNAPSHOT'
     return match_str

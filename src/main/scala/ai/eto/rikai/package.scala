74d4f31e5f63bbc80ce5b0f6f5393d28df8e2a94
Da <da@tubi.tv>
2021-04-07 08:17:39 +0800

    2020 -> 2021

--- src/main/scala/ai/eto/rikai/package.scala
+++ src/main/scala/ai/eto/rikai/package.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Rikai authors
+ * Copyright 2021 Rikai authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

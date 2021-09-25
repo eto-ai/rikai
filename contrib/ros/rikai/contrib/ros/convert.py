4fbde4d468dbbdc76ddf5b2fe39332342e4cb96e
Lei Xu <eddyxu@gmail.com>
2021-09-25 12:24:56 -0700

    use environment to setup a ros env

---
+++ contrib/ros/rikai/contrib/ros/convert.py
@@ -0,0 +1,139 @@
+#  Copyright (c) 2021 Rikai Authors
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import datetime
+import re
+from abc import ABC, abstractmethod
+from typing import Any, Optional, Tuple
+
+import genpy
+
+
+class Converter(ABC):
+    """Converter translates a ROS message or class to another type"""
+
+    @abstractmethod
+    def is_supported(self, message_type: str) -> bool:
+        pass
+
+    @abstractmethod
+    def convert(self, message_type: str, value: genpy.message.Message):
+        pass
+
+
+class JsonConverter(Converter):
+    """Convert ROS Message to Python Dict"""
+
+    CONVERT_MAP = {
+        "bool": bool,
+        "char": int,
+        "int8": int,
+        "int16": int,
+        "int32": int,
+        "int64": int,
+        # Converting the unsigned numbers might overflow.
+        "uint8": int,
+        "uint16": int,
+        "uint32": int,
+        "uint64": int,
+        "float32": float,
+        "float64": float,
+        "string": str,
+        "time": lambda rt: datetime.datetime.fromtimestamp(
+            rt.secs + rt.nsecs / 1e9
+        ),
+        "byte": int,
+        "duration": int,
+        "byte[]": bytearray,
+        "uint8[]": bytearray,
+    }
+
+    def is_convertable(
+        self,
+        msg_type: genpy.message.Message,
+    ) -> bool:
+        if isinstance(msg_type, str) and msg_type in self.CONVERT_MAP:
+            return True
+        return False
+
+    def convert(self, message_type, value: genpy.message.Message):
+        if value is None:
+            return None
+        if message_type in self.CONVERT_MAP:
+            return self.CONVERT_MAP[message_type](value)
+
+    def array_type(self, value):
+        if value is None:
+            return []
+
+        return list(value)
+
+
+class Visitor:
+    """Visitor walks through a ROS Message and converts it to another type system (i.e., Spark or Numpy)."""
+
+    def __init__(self, converter: Converter) -> None:
+        self.converter = converter
+
+    @staticmethod
+    def _parse_array(message_type: str) -> Optional[Tuple[str, Optional[int]]]:
+        """Parse array defination.
+
+        Parameters
+        ----------
+        message_type : str
+            ROS Message type, in str, i.e., `std_msgs/ByteMultiArray`.
+
+        Return
+        ------
+        A tuple of `[element type, array_length]` if it is an fixed-size array.
+        A tuple of `[element type, None]` if it is variable-length array.
+        Return None if it is not an array.
+        """
+        if not isinstance(message_type, str):
+            return None
+        matched = re.match(r"(.*)\[(\d+)?\]", message_type)
+        if matched:
+            return matched[1], matched[2]
+        return None
+
+    def visit(
+        self, message_type, value: Optional[genpy.message.Message] = None
+    ) -> Any:
+        if self.converter.is_convertable(message_type):
+            return self.converter.convert(message_type, value)
+
+        array_type_and_size = self._parse_array(message_type)
+        if array_type_and_size:
+            if value is None:
+                return self.converter.array_type([])
+            return self.converter.array_type(
+                [self.visit(array_type_and_size[0], v) for v in value]
+            )
+
+        if isinstance(
+            message_type, str
+        ):  # By this time this should be an object
+            assert "/" in message_type
+            message_type = _import_message_type(message_type)
+            return self.visit(message_type, value)
+
+        fields = {}
+        for field, field_type in _get_message_fields(message_type):
+            field_value = None
+            if value is not None:
+                field_value = getattr(value, field)
+
+            fields[field] = self.visit(field_type, field_value)
+        return fields
---
+++ contrib/ros/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="rikai",
+    version="0.0.1",
+    license="Apache License, Version 2.0",
+    author="Rikai authors",
+    author_email="rikai-dev@eto.ai",
+    url="https://github.com/eto-ai/rikai",
+)

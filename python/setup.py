b149d7395d236cb6ab5b5d19657586c7e4e01ac0
Renkai Ge <renkaige@tubi.tv>
2021-11-10 15:19:58 +0800

    upgrade pytorch

--- python/setup.py
+++ python/setup.py
@@ -24,7 +24,7 @@ dev = [
     "wheel",
 ]
 sklearn = ["scikit-learn"]
-torch = ["torch>=1.8.0", "torchvision>=0.11.0"]
+torch = ["torch>=1.8.1", "torchvision>=0.9.1"]
 jupyter = ["matplotlib", "jupyterlab"]
 aws = ["boto3", "botocore"]
 gcp = ["gcsfs"]

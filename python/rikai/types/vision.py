abc949c9733068198500d221d377f32f4bbc44a4
Darcy Shen <da@tubi.tv>
2021-11-22 14:37:13 +0800

    Tune image display on Databricks

--- python/rikai/types/vision.py
+++ python/rikai/types/vision.py
@@ -155,10 +155,15 @@ class Image(ToNumpy, ToPIL, Asset, Displayable, ToDict):
         -------
         img: IPython.display.Image
         """
-        from IPython.display import Image
+        from IPython.core.display import Image
 
-        with self.open() as fobj:
-            return Image(fobj.read(), **kwargs)
+        if not self.is_embedded and self.uri.startswith("http"):
+            return Image(url=self.uri)
+        else:
+            with self.open() as fobj:
+                encoded = base64.b64encode(fobj.read()).decode("utf-8")
+                url = f"data:image;base64,{encoded}"
+                return Image(url=url)
 
     def __repr__(self) -> str:
         if self.is_embedded:

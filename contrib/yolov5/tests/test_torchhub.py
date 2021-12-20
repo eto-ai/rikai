3e1d7f2016038dd2460e9061b0f040a2ab27a61d
Darcy Shen <da@tubi.tv>
2021-12-20 13:44:40 +0800

    minor changes

--- contrib/yolov5/tests/test_torchhub.py
+++ contrib/yolov5/tests/test_torchhub.py
@@ -1,3 +1,17 @@
+#  Copyright (c) 2021 Rikai Authors
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 from pathlib import Path
 
 from pyspark.sql import SparkSession
@@ -11,17 +25,17 @@ def test_yolov5(spark: SparkSession):
     spark.sql(
         f"""
 CREATE MODEL yolov5
-FLAVOR pytorch
+FLAVOR yolov5
 PREPROCESSOR 'rikai.contrib.yolov5.transforms.pre_processing'
 POSTPROCESSOR 'rikai.contrib.yolov5.transforms.post_processing'
 OPTIONS (device="cpu", batch_size=32)
 RETURNS {OUTPUT_SCHEMA}
-USING "torchhub:///ultralytics/yolov5:v5.0/yolov5s";
+USING "torchhub:///ultralytics/yolov5:v6.0/yolov5s";
     """
     )
     result = spark.sql(
         f"""
-    select ML_PREDICT(yolov5_way_2, '{image_path}') as pred
+    select ML_PREDICT(yolov5, '{image_path}') as pred
     """
     )
 
--- python/tests/spark/sql/codegen/test_torchhub_registry.py
+++ python/tests/spark/sql/codegen/test_torchhub_registry.py
@@ -1,13 +1,26 @@
+#  Copyright (c) 2021 Rikai Authors
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 from pyspark.sql import SparkSession
-from utils import check_ml_predict
 
 from rikai.contrib.torch.transforms.fasterrcnn_resnet50_fpn import (
     OUTPUT_SCHEMA,
 )
 
 
-def test_resnet50(spark: SparkSession):
-    spark.sql(
+def test_create_model(rikai_spark: SparkSession):
+    rikai_spark.sql(
         f"""
 CREATE MODEL resnet50
 FLAVOR pytorch
@@ -18,4 +31,4 @@ RETURNS {OUTPUT_SCHEMA}
 USING "torchhub:///pytorch/vision:v0.9.1/resnet50";
     """
     )
-    check_ml_predict(spark, "resnet50")
+    rikai_spark.sql("show models").show()

2427d8084a0720feeab86b4f4009b28055b1041d
Lei Xu <eddyxu@gmail.com>
2021-12-22 15:50:59 -0800

    Do not offer height/width when construct polygon

--- python/rikai/types/geometry.py
+++ python/rikai/types/geometry.py
@@ -514,7 +514,7 @@ class Mask(ToNumpy, ToDict):
         width: Optional[int] = None,
         mask_type: Mask.Type = Type.POLYGON,
     ):
-        if height is None or width is None:
+        if mask_type != Mask.Type.POLYGON and (height is None or width is None):
             raise ValueError(
                 "Must provide height and width for RLE or Polygon type"
             )
@@ -559,7 +559,7 @@ class Mask(ToNumpy, ToDict):
         )
 
     @staticmethod
-    def from_polygon(data: list[list[float]], height: int, width: int) -> Mask:
+    def from_polygon(data: list[list[float]]) -> Mask:
         """Build mask from a Polygon
 
         Parameters
@@ -572,9 +572,7 @@ class Mask(ToNumpy, ToDict):
         width: int
             The width of the image which the mask applies to.
         """
-        return Mask(
-            data, height=height, width=width, mask_type=Mask.Type.POLYGON
-        )
+        return Mask(data, mask_type=Mask.Type.POLYGON)
 
     @staticmethod
     def from_mask(mask: np.ndarray) -> Mask:

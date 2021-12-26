aece9a6e705e62747bfbebe024d8118ea84bb025
Renkai Ge <renkaige@tubi.tv>
2021-12-26 10:17:34 +0800

    add test for model version

--- python/tests/spark/sql/codegen/test_mlflow_logger.py
+++ python/tests/spark/sql/codegen/test_mlflow_logger.py
@@ -16,6 +16,8 @@ from pathlib import Path
 
 import mlflow
 import torch
+from mlflow.entities.model_registry import ModelVersion
+from mlflow.tracking import MlflowClient
 
 import rikai
 
@@ -40,3 +42,43 @@ def test_none_value_tags(tmp_path: Path, resnet_model_uri):
         assert tags[rikai.mlflow.CONF_MLFLOW_OUTPUT_SCHEMA] == "output_schema"
         assert rikai.mlflow.CONF_MLFLOW_PRE_PROCESSING not in tags
         assert rikai.mlflow.CONF_MLFLOW_POST_PROCESSING not in tags
+
+
+def test_model_and_version_tags(tmp_path: Path, resnet_model_uri):
+    tmp_path.mkdir(parents=True, exist_ok=True)
+    tracking_uri = "sqlite:///" + str(tmp_path / "tracking.db")
+    mlflow.set_tracking_uri(tracking_uri)
+    model_name = "model_for_tags"
+    c = MlflowClient()
+    c.create_registered_model(model_name)
+    with mlflow.start_run():
+        model = torch.load(resnet_model_uri)
+        rikai.mlflow.pytorch.log_model(
+            model,
+            "artifact_path",
+            "output_schema",
+            registered_model_name=model_name,
+            pre_processing=None,
+            post_processing=None,
+        )
+        run_id = mlflow.active_run().info.run_id
+        run = mlflow.get_run(run_id)
+        tags = run.data.tags
+
+    all_versions = c.get_latest_versions(
+        model_name, stages=["production", "staging", "None"]
+    )
+    current_version: ModelVersion = None
+    for v in all_versions:
+        if v.run_id == run_id:
+            current_version = v
+            break
+    if current_version is None:
+        raise ValueError(
+            "No model version found matching runid: {}".format(run_id)
+        )
+    print(f"current version {current_version}")
+    assert current_version.tags[rikai.mlflow.CONF_MLFLOW_ARTIFACT_PATH] == tags[rikai.mlflow.CONF_MLFLOW_ARTIFACT_PATH]
+    assert current_version.tags[rikai.mlflow.CONF_MLFLOW_OUTPUT_SCHEMA] == tags[rikai.mlflow.CONF_MLFLOW_OUTPUT_SCHEMA]
+    assert current_version.tags[rikai.mlflow.CONF_MLFLOW_MODEL_FLAVOR] == 'pytorch'
+

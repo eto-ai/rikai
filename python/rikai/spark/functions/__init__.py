c4f2fb929e0b5115fba72eb6e1f339a0fc98063b
Darcy Shen <66561265+da-tubi@users.noreply.github.com>
2021-12-28 09:36:50 +0800

    Register all rikai UDFs using init (#359)
    * Register all rikai UDFs using init
    
    * test init
    
    * test init
    
    * revert
    
    * manually register
    
    * Fix

--- python/rikai/spark/functions/__init__.py
+++ python/rikai/spark/functions/__init__.py
@@ -15,6 +15,38 @@
 """Domain-specific Pyspark UDFs
 """
 
+from pyspark.sql import SparkSession
+
 from rikai.spark.functions.geometry import *
 from rikai.spark.functions.io import *
 from rikai.spark.functions.vision import *
+
+__all__ = ["init"]
+
+
+def init(spark: SparkSession):
+    """Register all rikai UDFs"""
+    from rikai.spark.functions import geometry, io, vision
+
+    all_geo_udfs = [
+        "area",
+        "box2d",
+        "box2d_from_center",
+        "box2d_from_top_left",
+    ]
+    all_io_udfs = ["copy"]
+    all_vis_udfs = [
+        "crop",
+        "to_image",
+        "image_copy",
+        "numpy_to_image",
+        "video_to_images",
+        "spectrogram_image",
+        "video_metadata",
+    ]
+    for name in all_geo_udfs:
+        spark.udf.register(name, getattr(geometry, name))
+    for name in all_io_udfs:
+        spark.udf.register(name, getattr(io, name))
+    for name in all_vis_udfs:
+        spark.udf.register(name, getattr(vision, name))
--- python/tests/spark/test_functions.py
+++ python/tests/spark/test_functions.py
@@ -33,17 +33,31 @@ from rikai.spark.functions import (
     area,
     box2d,
     box2d_from_center,
+    crop,
     image_copy,
+    init,
     numpy_to_image,
     spectrogram_image,
     to_image,
     video_metadata,
     video_to_images,
 )
-from rikai.spark.functions.vision import crop
 from rikai.spark.types.geometry import Box2dType
 from rikai.spark.types.vision import ImageType
 from rikai.types import Box2d, Image, Segment, VideoStream, YouTubeVideo
+
+
+def test_init(spark):
+    init(spark)
+    rikai_udf_names = [
+        x.name
+        for x in spark.catalog.listFunctions()
+        if x.className.startswith("org.apache.spark.sql.UDFRegistration")
+    ]
+    assert "area" in rikai_udf_names
+    assert "copy" in rikai_udf_names
+    assert "to_image" in rikai_udf_names
+    assert len(rikai_udf_names) > 10
 
 
 def assert_area_equals(array, df):

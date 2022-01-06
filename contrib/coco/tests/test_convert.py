9722d64d246f12022b454c8e7c96d07fd94b189b
Lei Xu <eddyxu@gmail.com>
2022-01-06 14:32:20 -0800

    fix test coco

--- contrib/coco/tests/test_convert.py
+++ contrib/coco/tests/test_convert.py
@@ -32,7 +32,9 @@ def test_covert_segmentation():
         ann_ids = coco.getAnnIds(imgIds=image_id)
         for ann in coco.loadAnns(ann_ids):
             if ann["iscrowd"] == 0:
-                mask = Mask.from_polygon(ann["segmentation"])
+                mask = Mask.from_polygon(
+                    ann["segmentation"], height=height, width=width
+                )
                 # TODO: currently, it has disparity of the conversion between
                 # polygon to mask between coco and rikai (PIL based)
             else:

5134acf41427ba46540b3cae04b692f4d1d6123e
Darcy Shen <da@tubi.tv>
2022-01-07 11:50:31 +0800

    pre_processing comments

--- python/rikai/contrib/torchhub/pytorch/vision/resnet.py
+++ python/rikai/contrib/torchhub/pytorch/vision/resnet.py
@@ -28,6 +28,13 @@ def _ndarray_to_pil(image):
 
 
 def pre_processing(options: Dict[str, Any]) -> Callable:
+    '''
+    All pre-trained models expect input images normalized in the same way, i.e.
+    mini-batches of 3-channel RGB images of shape (3 x H x W), where H and W
+    are expected to be at least 224. The images have to be loaded in to a range
+    of [0, 1] and then normalized using mean = [0.485, 0.456, 0.406] and std =
+    [0.229, 0.224, 0.225].
+    '''
     return T.Compose(
         [
             _ndarray_to_pil,

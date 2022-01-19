d2c9e2f424ef03cff6ceeed22bb8a4c9bb29baec
Lei Xu <eddyxu@gmail.com>
2022-01-18 16:08:10 -0800

    fix scala 2.13 build

--- src/main/scala/ai/eto/rikai/sql/spark/RikaiSparkSessionExtensions.scala
+++ src/main/scala/ai/eto/rikai/sql/spark/RikaiSparkSessionExtensions.scala
@@ -1,4 +1,6 @@
 /*
+ * Copyright 2022 Rikai authors
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,6 +19,7 @@ package ai.eto.rikai.sql.spark
 import ai.eto.rikai.sql.model.{Catalog, ModelSpec, Registry}
 import ai.eto.rikai.sql.spark.expressions.Predict
 import ai.eto.rikai.sql.spark.parser.{RikaiExtSqlParser, RikaiSparkSQLParser}
+import com.thoughtworks.enableIf
 import org.apache.spark.sql.catalyst.FunctionIdentifier
 import org.apache.spark.sql.catalyst.analysis.{
   UnresolvedAttribute,
@@ -37,39 +40,61 @@ private class MlPredictRule(val session: SparkSession)
 
   val catalog = Catalog.getOrCreate(session)
 
+  private def resolveModel(
+      name: String,
+      arguments: Seq[Expression]
+  ): Expression = {
+    if (arguments.size < 2) {
+      throw new UnsupportedOperationException(
+        s"${name.toUpperCase} requires at least 2 parameters" +
+          s", got ${arguments.size}"
+      )
+    }
+
+    val model_name = arguments.head
+    val model = model_name match {
+      case arg: UnresolvedAttribute =>
+        catalog.getModel(arg.name, session)
+      case arg: Literal => {
+        val model =
+          Registry.resolve(
+            session,
+            ModelSpec(name = None, uri = arg.toString)
+          )
+        Some(model)
+      }
+    }
+    model match {
+      case Some(r: SparkRunnable) => {
+        r.asSpark(arguments.drop(1))
+      }
+      case _ =>
+        throw new UnsupportedOperationException("Unsupported model")
+    }
+  }
+
+  @enableIf(
+    scala.util.Properties.versionNumberString.compareTo("2.12.15") >= 0
+  )
+  private def getFuncName(f: UnresolvedFunction): String = {
+    // After Spark 3.2
+    f.nameParts.last
+  }
+
+  @enableIf(
+    scala.util.Properties.versionNumberString.compareTo("2.12.15") < 0
+  )
+  private def getFuncName(f: UnresolvedFunction): String = {
+    f.name.funcName
+  }
+
   override def apply(plan: LogicalPlan): LogicalPlan = {
     plan.resolveExpressions {
-      case f: UnresolvedFunction
-          if f.name.funcName.toLowerCase() == "ml_predict" => {
-        println(s"FUNCTION: ${f}")
-        val arguments = f.arguments
-        if (arguments.size < 2) {
-          throw new UnsupportedOperationException(
-            s"${f.name.funcName.toUpperCase} requires at least 2 parameters" +
-              s", got ${arguments.size}"
-          )
-        }
-
-        val model_name = arguments.head
-        println(s"MODEL NAME: ${model_name}")
-        val model = model_name match {
-          case arg: UnresolvedAttribute =>
-            catalog.getModel(arg.name, session)
-          case arg: Literal => {
-            val model =
-              Registry.resolve(
-                session,
-                ModelSpec(name = None, uri = arg.toString)
-              )
-            Some(model)
-          }
-        }
-        model match {
-          case Some(r: SparkRunnable) => {
-            r.asSpark(arguments.drop(1))
-          }
-          case _ =>
-            throw new UnsupportedOperationException("Unsupported model")
+      case f: UnresolvedFunction => {
+        val funcName = getFuncName(f)
+        funcName.toLowerCase match {
+          case "ml_predict" => resolveModel(funcName, f.arguments)
+          case _            => f
         }
       }
     }

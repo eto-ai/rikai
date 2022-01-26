c71c312a91b471096f0ffb280a4db6373942cb60
Lei Xu <eddyxu@gmail.com>
2022-01-25 22:20:22 -0800

    add test

---
+++ python/rikai/contrib/torch/inspect/ssd.py
@@ -0,0 +1,124 @@
+#  Copyright 2022 Rikai Authors
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+"""Inspect detail of Torchvision SSD model
+
+https://pytorch.org/vision/stable/models.html#torchvision.models.detection.ssd300_vgg16
+
+"""
+
+from typing import List
+
+import torch
+import torch.nn.functional as F
+from torchvision.ops.boxes import batched_nms, clip_boxes_to_image
+from torchvision.models.detection.ssd import SSD
+
+
+__all__ = ["SSDClassScoresExtractor"]
+
+
+class SSDClassScoresExtractor(torch.nn.Module):
+    """Extracts the score (confidence) of each class for
+    all the detected bounding box.
+
+    Parameters
+    ----------
+    backend : :class:`torch.nn.Module`
+        The trained SSD model
+    topk_candidates : int, optional
+        The number of top candidates (classes) returned per box.
+
+    Returns
+    -------
+    """
+
+    SCHEMA = "array<struct<box:box2d, scores:array<float>, labels:array<int>>>"
+
+    def __init__(self, backend: SSD, topk_candidates: int = 2):
+        super().__init__()
+        if not isinstance(backend, SSD):
+            raise ValueError("Only support Torchvision's SSD model.")
+        self.backend = backend
+        self.topk_candidates = topk_candidates
+
+    def forward(
+        self,
+        images: List[torch.Tensor],
+    ):
+        if self.training:
+            raise ValueError("This feature extractor only supports eval mode.")
+
+        images, _ = self.backend.transform(images, None)
+
+        # Get the features from the backbone
+        features = self.backend.backbone(images.tensors)
+        features = list(features.values())
+        head_outputs = self.backend.head(features)
+        anchors = self.backend.anchor_generator(images, features)
+
+        scores = head_outputs["cls_logits"]
+        bbox_regression = head_outputs["bbox_regression"]
+        pred_scores = F.softmax(scores, dim=-1)
+
+        num_classes = pred_scores.size(-1)
+        device = pred_scores.device
+
+        detections = []
+        for boxes, scores, anchors, image_shape in zip(
+            bbox_regression, pred_scores, anchors, images.image_sizes
+        ):
+            boxes = self.backend.box_coder.decode_single(boxes, anchors)
+            boxes = clip_boxes_to_image(boxes, image_shape)
+
+            image_boxes = []
+            image_scores = []
+            image_labels = []
+            image_all_scores = []
+            for label in range(1, num_classes):
+                score = scores[:, label]
+
+                keep_idxs = score > self.backend.score_thresh
+                score = score[keep_idxs]
+                all_scores = scores[keep_idxs]
+                box = boxes[keep_idxs]
+                # keep only topk scoring predictions
+                num_topk = min(self.backend.topk_candidates, score.size(0))
+                score, idxs = score.topk(num_topk)
+                box = box[idxs]
+                all_scores = all_scores[idxs]
+
+                image_boxes.append(box)
+                image_scores.append(score)
+                image_all_scores.append(all_scores)
+
+            image_boxes = torch.cat(image_boxes, dim=0)
+            image_scores = torch.cat(image_scores, dim=0)
+            image_labels = torch.cat(image_labels, dim=0)
+            image_all_scores = torch.cat(image_all_scores, dim=0)
+
+            # non-maximum suppression
+            keep = batched_nms(
+                image_boxes,
+                image_scores,
+                image_labels,
+                self.backend.nms_thresh,
+            )
+            keep = keep[: self.backend.detections_per_img]
+            # print(keep)
+            print(image_boxes[keep], scores.shape, scores[keep, 1:].shape)
+            print(scores[keep, 1:][0])
+            print(image_all_scores[keep], image_all_scores[keep].shape)
+            print(image_all_scores[keep, 1:].topk(2))
+        return detections

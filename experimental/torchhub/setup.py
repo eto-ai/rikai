d4bc60ff9617704fda00866ad9d27684ebc3990d
Darcy Shen <da@tubi.tv>
2022-02-15 14:53:50 +0800

    rikai-torchhub 0.0.2

--- experimental/torchhub/setup.py
+++ experimental/torchhub/setup.py
@@ -2,13 +2,13 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name="rikai-torchhub",
-    version="0.0.1",
+    version="0.0.2",
     license="Apache License, Version 2.0",
     author="Rikai authors",
     author_email="rikai-dev@eto.ai",
     url="https://github.com/eto-ai/rikai",
     python_requires=">=3.7",
-    install_requires=["rikai>=0.0.20", "torch", "torchvision"],
+    install_requires=["rikai>=0.1.2", "torch", "torchvision"],
     extras_require={
         "dev": [
             "black",

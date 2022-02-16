a8f5312db389da00fd0d069e2bb6cc7edefaf15d
Darcy Shen <da@tubi.tv>
2022-02-16 21:11:29 +0800

    model type unit test draft

--- experimental/torchhub/tests/test_resnet.py
+++ experimental/torchhub/tests/test_resnet.py
@@ -13,18 +13,43 @@
 #  limitations under the License.
 
 from pathlib import Path
+from rikai.types.vision import Image
 
 import torchvision
+import pandas as pd
 from pyspark.sql import SparkSession
 
 from rikai.spark.functions import to_image
+from rikai.experimental.torchhub.torchhub_registry import (
+    TorchHubRegistry,
+)
+from rikai.spark.sql.codegen.base import func_from_spec
+
+
+version = f"v{torchvision.__version__.split('+', maxsplit=1)[0]}"
+work_dir = Path().absolute().parent.parent
+image_path = f"{work_dir}/python/tests/assets/test_image.jpg"
+
+
+def test_resnet_model_type():
+    reg = TorchHubRegistry()
+    spec = reg.make_model_spec(
+        {
+            "schema": None,
+            "modelType": None,
+            "flavor": None,
+            "name": "resnet50",
+            "uri": f"torchhub:///pytorch/vision:{version}/resnet50",
+        }
+    )
+    func = func_from_spec(spec)
+    input = [pd.Series(Image(image_path))]
+    results_list = [results for results in func(input)]
+    assert len(results_list[0][0]) == 1000
 
 
 def test_resnet(spark: SparkSession):
     spark.udf.register("to_image", to_image)
-    work_dir = Path().absolute().parent.parent
-    image_path = f"{work_dir}/python/tests/assets/test_image.jpg"
-    version = f"v{torchvision.__version__.split('+', maxsplit=1)[0]}"
     for n in ["18", "34", "50", "101", "152"]:
         spark.sql(
             f"""

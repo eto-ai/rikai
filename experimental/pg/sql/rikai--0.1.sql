bdeb68697d1865128c7d6d20b9c370672252454c
Lei Xu <eddyxu@gmail.com>
2022-05-05 23:18:12 -0700

    add detection function

--- experimental/pg/sql/rikai--0.1.sql
+++ experimental/pg/sql/rikai--0.1.sql
@@ -1,4 +1,4 @@
-CREATE TYPE image AS (uri TEXT, data bytea);
+CREATE TYPE image AS (uri TEXT);
 
 
 CREATE FUNCTION ml_version()
@@ -12,8 +12,23 @@ $$ LANGUAGE plpython3u;
 CREATE TYPE detection AS (label TEXT, box box, score real);
 
 CREATE FUNCTION ssd(img image)
-RETURNS detection[]
+RETURNS SETOF detection
 AS $$
+	plpy.info("Image is:", img)
 	return [{"label": "ssd", "box": ((1, 2), (3, 4)), "score": 0.85}]
 $$ LANGUAGE plpython3u;
 
+CREATE TABLE models (
+	name VARCHAR(128) PRIMARY KEY,
+	flavor VARCHAR(128),
+	model_type VARCHAR(128),
+	options JSONB
+);
+CREATE INDEX IF NOT EXISTS model_flavor_idx
+ON models (flavor, model_type);
+
+CREATE FUNCTION ml_detection(model TEXT, img image)
+RETURNS SETOF detection
+AS $$
+	return []
+$$ LANGUAGE plpython3u;

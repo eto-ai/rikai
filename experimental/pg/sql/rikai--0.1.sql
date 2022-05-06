55103542724a1927f4961e6179b7bdfadd82ccf2
Lei Xu <eddyxu@gmail.com>
2022-05-06 13:08:35 -0700

    add label id

--- experimental/pg/sql/rikai--0.1.sql
+++ experimental/pg/sql/rikai--0.1.sql
@@ -4,15 +4,16 @@ CREATE SCHEMA IF NOT EXISTS ml;
 -- Semantic Types
 CREATE TYPE image AS (uri TEXT);
 
-CREATE TYPE detection AS (label TEXT, box box, score real);
+CREATE TYPE detection AS (label TEXT, label_id int, box box, score real);
 
 
 -- Tables for ML metadata
 CREATE TABLE ml.models (
-	name VARCHAR(128) PRIMARY KEY,
-	flavor VARCHAR(128),
-	model_type VARCHAR(128),
-	options JSONB
+	name VARCHAR(128) NOT NULL PRIMARY KEY,
+	flavor VARCHAR(128) NOT NULL,
+	model_type VARCHAR(128) NOT NULL,
+	uri VARCHAR(1024),
+	options JSONB DEFAULT '{}'::json
 );
 CREATE INDEX IF NOT EXISTS model_flavor_idx
 ON ml.models (flavor, model_type);
@@ -25,17 +26,19 @@ AS $$
 	return rikai.__version__.version
 $$ LANGUAGE plpython3u;
 
+-- Trigger to create a model inference function after
+-- creating a model entry.
 CREATE FUNCTION ml.create_model_trigger()
 RETURNS TRIGGER
 AS $$
-	plpy.info("Creating model: ", TD)
 	model_name = TD["new"]["name"]
+	plpy.info("Creating model: ", model_name)
 	flavor = TD["new"]["flavor"]
 	stmt = (
 		"CREATE FUNCTION ml.{}(img image) ".format(model_name) +
 		"RETURNS detection[] " +
 		"AS $BODY$ " +
-		"	return None " +
+		"	return [{'label': 'haha', 'label_id': 50, 'box': ((1, 2), (3, 4)), 'score': 123.0}] " +
 		"$BODY$ LANGUAGE plpython3u;"
 	)
 	plpy.execute(stmt)

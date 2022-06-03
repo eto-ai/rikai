e91d21a81fde620d1a09d7e16d1d8cf94649b0a3
Lei Xu <eddyxu@gmail.com>
2022-06-02 21:20:27 -0700

    seriallize width/height with polygon

--- src/test/scala/org/apache/spark/sql/rikai/MaskTest.scala
+++ src/test/scala/org/apache/spark/sql/rikai/MaskTest.scala
@@ -33,7 +33,7 @@ class MaskTest extends AnyFunSuite with SparkTestSession {
     val df =
       Seq(
         (1, Mask.fromRLE(Array(1, 2, 3), 10, 5)),
-        (2, Mask.fromPolygon(Array(Array(1, 1, 5, 5, 10, 10))))
+        (2, Mask.fromPolygon(Array(Array(1, 1, 5, 5, 10, 10)), 5, 20))
       ).toDF("id", "segmentation")
 
     df.write.format("rikai").save(testDir.toString)
@@ -42,5 +42,9 @@ class MaskTest extends AnyFunSuite with SparkTestSession {
     assert(df.count() == actualDf.count())
     assert(df.exceptAll(actualDf).isEmpty)
     df.show()
+
+    val polygon = actualDf.filter("id = 2").first()
+    assert(polygon.getAs[Mask]("segmentation").width == 5);
+    assert(polygon.getAs[Mask]("segmentation").height == 20);
   }
 }

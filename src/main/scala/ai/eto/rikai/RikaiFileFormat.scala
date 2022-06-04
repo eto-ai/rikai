be759a34a1d77f431c5d7e55df8f60d568dca7c6
Renkai Ge <renkaige@tubi.tv>
2022-06-04 17:58:13 +0800

    change conf place

--- src/main/scala/ai/eto/rikai/RikaiFileFormat.scala
+++ src/main/scala/ai/eto/rikai/RikaiFileFormat.scala
@@ -1,6 +1,8 @@
 package ai.eto.rikai
+import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.mapreduce.Job
+import org.apache.parquet.hadoop.util.ContextUtil
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.execution.datasources.OutputWriterFactory
 import org.apache.spark.sql.execution.datasources.parquet.ParquetFileFormat
@@ -10,11 +12,9 @@ import org.json4s.jackson.Serialization
 
 class RikaiFileFormat extends ParquetFileFormat {
   private def setSparkOptions(
-      sparkSession: SparkSession,
+      conf: Configuration,
       options: RikaiOptions
   ): Unit = {
-    val conf = sparkSession.sparkContext.hadoopConfiguration
-    // conf.set("parquet.summary.metadata.level", "ALL")
     conf.setInt("parquet.page.size.row.check.min", 3)
     conf.setInt("parquet.page.size.row.check.max", 32)
     conf.setInt("parquet.block.size", options.blockSize)
@@ -44,8 +44,9 @@ class RikaiFileFormat extends ParquetFileFormat {
       options: Map[String, String],
       dataSchema: StructType
   ): OutputWriterFactory = {
-    val rikaiOptions = new RikaiOptions(options.toSeq)
-    setSparkOptions(sparkSession, rikaiOptions)
+    val rikaiOptions = RikaiOptions(options.toSeq)
+    val conf: Configuration = ContextUtil.getConfiguration(job)
+    setSparkOptions(conf, rikaiOptions)
 
     /** Rikai metadata directory. */
     val rikaiDir = new Path(rikaiOptions.path, "_rikai")

8bc3ec4287c18ec2a54bef0fd86f2666795d344d
Lei Xu <lei@eto.ai>
2022-06-08 09:43:45 -0700

    add test for efficientnet

--- python/tests/pytorch/models/test_feature_extractor.py
+++ python/tests/pytorch/models/test_feature_extractor.py
@@ -17,7 +17,7 @@ from pathlib import Path
 import torch
 from pyspark.sql import SparkSession
 from pyspark.sql.types import ArrayType, FloatType, StructField, StructType
-from torchvision.models import resnet50
+from torchvision.models import resnet50, efficientnet_b0
 
 from rikai.pytorch.models.feature_extractor import FeatureExtractor
 
@@ -33,7 +33,7 @@ def test_resnet_features(
 
     spark.sql(
         f"""
-    CREATE MODEL resnet_features
+    CREATE OR REPLACE MODEL resnet_features
     FLAVOR pytorch
     MODEL_TYPE feature_extractor
     OPTIONS (model_type = 'resnet')
@@ -50,3 +50,33 @@ def test_resnet_features(
         [StructField("embedding", ArrayType(FloatType()))]
     )
     assert len(df.first().embedding) == 2048
+
+
+def test_efficientnet_features(
+    tmp_path: Path, spark: SparkSession, asset_path: Path
+):
+    effnet = efficientnet_b0(pretrained=True)
+
+    extractor = FeatureExtractor(effnet, "avgpool")
+    uri = str(tmp_path / "extractor.pth")
+    torch.save(extractor, uri)
+
+    spark.sql(
+        f"""
+        CREATE OR REPLACE MODEL effnet_features
+        FLAVOR pytorch
+        MODEL_TYPE feature_extractor
+        OPTIONS (model_type = 'efficientnet_b0')
+        USING '{uri}'
+        """
+    )
+    df = spark.sql(
+        f"""SELECT ML_PREDICT(
+                effnet_features, image('{asset_path / 'cat.jpg'}')
+            ) as embedding"""
+    )
+
+    assert df.schema == StructType(
+        [StructField("embedding", ArrayType(FloatType()))]
+    )
+    assert len(df.first().embedding) == 1280

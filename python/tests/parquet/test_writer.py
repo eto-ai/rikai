eec9460542a2858f7e9d584b219f4c38a78099c5
Chang She <changshe@gmail.com>
2022-06-20 14:37:55 -0700

    Fix pandas writer bug (#686)
    * Fix bug converting array type
    
    * fix pandas rikai writer handling of array of simple types
    
    * black
    
    Co-authored-by: Chang She <cshe@tubi.tv>

--- python/tests/parquet/test_writer.py
+++ python/tests/parquet/test_writer.py
@@ -101,6 +101,7 @@ def _make_df(nrows=1):
         fields=[
             StructField("image_id", StringType()),
             StructField("image", ImageType()),
+            StructField("image_labels", ArrayType(elementType=StringType())),
             StructField("split", StringType()),
             StructField(
                 "annotations",
@@ -123,6 +124,7 @@ def _make_df(nrows=1):
             {
                 "image_id": str(i),
                 "image": Image(f"s3://bucket/path_{i}.jpg"),
+                "image_labels": ["foo", "bar"],
                 "split": splits[random.randint(0, len(splits) - 1)],
                 "annotations": [
                     {
